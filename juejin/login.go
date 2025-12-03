34c8e99cbf4d2864347877c5cbb0f5ed285b2dc0
pkc918 <xiaopan2000918@gmail.com>
2025-12-03 22:54:02 +0800

    refactor: extract constants for login URL and elements in login.go

--- juejin/login.go
+++ juejin/login.go
@@ -9,14 +9,20 @@ import (
 	"github.com/go-rod/rod"
 )
 
+var (
+	LOGIN_URL    = "https://juejin.cn/"
+	LOGIN_BUTTON = `//*[@id="juejin"]/div[1]/div/header/div/nav/ul/ul/li[2]/div/button`
+	USERAVATAR   = `#juejin > div.container.index-container > div > header > div > nav > ul > ul > li.nav-item.menu > div > div > img`
+)
+
 func Login(page *rod.Page, ctx context.Context) error {
 	p := page.Context(ctx)
-	p.MustNavigate("https://juejin.cn/").MustWaitLoad()
-	page.MustElementX(`//*[@id="juejin"]/div[1]/div/header/div/nav/ul/ul/li[2]/div/button`).MustClick()
+	p.MustNavigate(LOGIN_URL).MustWaitLoad()
+	page.MustElementX(LOGIN_BUTTON).MustClick()
 
 	time.Sleep(5 * time.Second)
 
-	if exist, el, _ := p.Has("#juejin > div.container.index-container > div > header > div > nav > ul > ul > li.nav-item.menu > div > div > img"); exist {
+	if exist, el, _ := p.Has(USERAVATAR); exist {
 		fmt.Println(el.Describe(0, true))
 		return nil
 	}
